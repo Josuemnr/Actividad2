287e799948315319e44a5568c0d0ee8a19a36929
AnaHerrera <a01666336@tec.mx>
2025-03-20 19:06:09 -0600

    La serpiente y la comida cambiaran de color de forma aleatoria cada vez que incie una partida

--- snake.py
+++ snake.py
@@ -1,10 +1,13 @@
 from turtle import *
-from random import randrange
+from random import randrange, choice
 from freegames import square, vector
 
 food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
+colors_list = ('black', 'green', 'darkorange', 'navy', 'blue')
+color_snake = None
+color_food = None
 
 def change(x, y):
     "Change snake direction."
@@ -24,6 +27,17 @@ def move_food():
         food.move(move_direction)
 
     ontimer(move_food,1000)
+
+def color():
+    global color_snake, color_food
+    color_snake = choice(colors_list)  
+    color_food = choice(colors_list)
+
+    # Asegurar que la comida y la serpiente no tengan el mismo color
+    while color_food == color_snake:
+        color_food = choice(colors_list)
+
+
 def move():
     "Move snake forward one segment."
     head = snake[-1].copy()
@@ -44,11 +58,14 @@ def move():
         snake.pop(0)
 
     clear()
+    
 
     for body in snake:
-        square(body.x, body.y, 9, 'black')
+        square(body.x, body.y, 9, color_snake)
 
-    square(food.x, food.y, 9, 'green')
+    
+    
+    square(food.x, food.y, 9, color_food)
     update()
     ontimer(move, 100)
 
@@ -62,6 +79,8 @@ onkey(lambda: change(10, 0), 'Right')
 onkey(lambda: change(-10, 0), 'Left')
 onkey(lambda: change(0, 10), 'Up')
 onkey(lambda: change(0, -10), 'Down')
+
+color()
 move()
 move_food()
 done()
